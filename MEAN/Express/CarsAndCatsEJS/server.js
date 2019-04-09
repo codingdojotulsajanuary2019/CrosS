1d14e66e1ede3d411c1cc1e9e61e06e3990dd3e3
Cros Sikes <crossikes@gmail.com>
2019-04-09 12:58:55 -0500

    My CatInfo Push

--- MEAN/Express/CarsAndCatsEJS/server.js
+++ MEAN/Express/CarsAndCatsEJS/server.js
@@ -11,6 +11,30 @@ app.get('/cars', function(request, response){
 app.get('/cats', function(request, response){
     response.render('cats');
 })
+
+
+
+app.get('/cat/joe', function(request, response){
+    var cats_array = [
+    {name: "Joe", age: 2, food: "Kibbles", sleep: "Under the couch"}
+    ];
+    response.render('catinfo', {cats: cats_array});
+})
+app.get('/cat/sam', function(request, response){
+    var cats_array2 = [
+    {name: "Sam", age: 4, food: "Meow Mix", sleep: "On the couch"}
+    ];
+    response.render('catinfo', {cats: cats_array2});
+})
+app.get('/cat/jim', function(request, response){
+    var cats_array3 = [
+    {name: "Jim", age: 1, food: "Fancy Feast", sleep: "In the Window Sill"}
+    ];
+    response.render('catinfo', {cats: cats_array3});
+})
+
+
+
 app.get('/cars/new', function(request, response){
     response.render('form');
 })
