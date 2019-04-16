374bdbb853f523565bb7d9b78356f0d8b65f25b1
Cros Sikes <crossikes@gmail.com>
2019-04-16 12:02:11 -0500

    Did I do it? Push

---
+++ MEAN/Angular/sample_app/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var path = require('path');
+
+app.use(express.static(__dirname + '/public/dist/public'));
+
+app.set('views', path.join(__dirname, '/views'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response){
+    response.render('index');
+})
+
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+});
